6e1ebeec42285b5049a56a495fe6ad8e63c1f4c3
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2022-05-24 12:18:41 -0400

    Initial Commit

---
+++ SwiftUISeries-Animations/SwiftUISeries_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISeries_AnimationsApp.swift
+//  SwiftUISeries-Animations
+//
+//  Created by Kuba Szulaczkowski on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISeries_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
